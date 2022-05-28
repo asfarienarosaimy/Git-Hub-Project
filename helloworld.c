6f34aaffe2e28181618f7f65b34e06b3908ee99b
nurasfariena <s21a0043@siswa.umk.edu.my>
2022-05-28 15:57:44 +0800

    update helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -1,11 +1,6 @@
 #include<stdio.h>
 int main(){
 printf("Hello World!");
-}
+printf("This is my first repository");
 
-
-#include<stdio.h>
-int main(){
-printf("Hello World!");
-printf("This is my first repository");  
 }
